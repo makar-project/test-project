079ebf65bc178e9768fbf94adc92d6da7d54944a
qdyyys <qdyyys@gmail.com>
2024-03-14 20:27:44 +0300

    quest2

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@
 
 
 function uniqueNumbers(numbers) {
-    let numbersOfUnique = new Set(numbers);
-    return numbersOfUnique
+    let uniqueNumbers = [...new Set(numbers)]
+    return uniqueNumbers
   }
 const numbers = [1, 2, 2, 3, 4, 4, 5];
 console.log(uniqueNumbers(numbers)); // [1, 2, 3, 4, 5]
@@ -12,11 +12,13 @@ console.log(uniqueNumbers(numbers)); // [1, 2, 3, 4, 5]
 
 //* Предположим, у вас есть два массива, представляющие два разных набора данных (например, список имен). Необходимо написать функцию, которая возвращает массив, содержащий только те элементы, которые присутствуют в обоих массивах (пересечение массивов). Используйте Set для решения этой задачи.
 function findCommonElements(arr1, arr2) {
-    let commonNames = new Set(arr1);
-    for (let name of array2) {
-        commonNames.add(name)
-    }
-    return commonNames
+  let set1 = new Set(arr1)
+  let set2 = new Set(arr2)
+  let commonElements;
+  commonElements = new Set([...set1].filter(function(element){
+    return set2.has(element)
+  }));
+  return [...commonElements]
 }
 
 const array1 = ['Alice', 'Bob', 'Charlie', 'Diana'];
