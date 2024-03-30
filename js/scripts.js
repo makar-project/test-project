770c7b63913a4e118d541bccb27d0ab356670c8b
qdyyys <qdyyys@gmail.com>
2024-03-30 20:51:08 +0300

    quest

--- js/scripts.js
+++ js/scripts.js
@@ -34,3 +34,45 @@ libs.addbook('"Гарри Поттер", "Дж. К. Роулинг", 1997')
 console.log(libs.showBooks())
 console.log(libs.getSummary(1))
 
+
+
+
+//                                     Quest2
+class Vehicle {
+    constructor(make, year) {
+        this.make = make;
+        this.year = year;
+    }
+    info() {
+        return `Make: ${this.make} Year: ${this.year}`
+    }
+    age() {
+        let currentYear = 2024;
+        return `Возраст транспорта: ${currentYear - this.year} лет`
+    }
+}
+
+class Truck extends Vehicle {
+    constructor(make, year, payload) {
+        super(make,year)
+        this.payload = payload;
+    }
+    info() {
+        let currentYear = 2024;
+        return `Make: ${this.make}, Возраст транспорта: ${currentYear - this.year} лет, Грузоподъемность: ${this.payload}kg`
+    }
+}
+
+const myVehicle = new Vehicle("Toyota", 2010);
+console.log(myVehicle.info());
+console.log(myVehicle.age());
+const myTruck = new Truck("Volvo", 2015, 5000);
+console.log(myTruck.info()); 
+console.log(myTruck.age());
+
+
+
+
+
+
+
